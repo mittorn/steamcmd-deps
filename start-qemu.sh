34f2eb03467479cadd6a5635c60cbf8943e0aaa6
Mittorn <mittorn@sibmail.com>
2016-02-25 22:17:05 +0000

    Qemu start script

---
+++ start-qemu.sh
@@ -0,0 +1,9 @@
+export LD_LIBRARY_PATH=$1/lib:$1/linux32
+export LD_PRELOAD=$1/resolvconf-override.so
+QEMU=$2
+LD_LINUX=$1/linux32/ld-linux.so.2
+export NAMESERVER1=8.8.8.8
+export HOME=$1
+cd $1
+shift 2
+$QEMU $LD_LINUX $*
