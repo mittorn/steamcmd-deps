6882787e9fe6d575575b5451f9770d682c6f88c3
mittorn <mittorn@sibmail.com>
2016-04-15 02:55:53 +0600

    Get nameserver from net.dns1

--- start-qemu.sh
+++ start-qemu.sh
@@ -1,8 +1,9 @@
+export NAMESERVER1=`getprop net.dns1` 2>/dev/null
+export NAMESERVER2=8.8.8.8
 export LD_LIBRARY_PATH=$1/lib:$1/linux32
 export LD_PRELOAD=$1/preload.so
 QEMU=$2
 LD_LINUX=$1/linux32/ld-linux.so.2
-export NAMESERVER1=8.8.8.8
 export HOME=$1
 cd $1
 shift 2
--- start-x86.sh
+++ start-x86.sh
@@ -1,7 +1,8 @@
+export NAMESERVER1=`getprop net.dns1` 2>/dev/null
+export NAMESERVER2=8.8.8.8
 export LD_LIBRARY_PATH=$1/lib:$1/linux32
-export LD_PRELOAD=$1/resolvconf-override.so
+export LD_PRELOAD=$1/preload.so
 LD_LINUX=$1/linux32/ld-linux.so.2
-export NAMESERVER1=8.8.8.8
 export HOME=$1
 cd $1
 shift 1
